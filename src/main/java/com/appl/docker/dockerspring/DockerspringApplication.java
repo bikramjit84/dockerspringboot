e8c0cfb6af0b121ad56286a14ac31a4b49efc1ce
Sanjenbam Bikramjit <you@example.com>
2022-08-10 11:29:21 -0500

    Added image

--- src/main/java/com/appl/docker/dockerspring/DockerspringApplication.java
+++ src/main/java/com/appl/docker/dockerspring/DockerspringApplication.java
@@ -1,9 +1,12 @@
 package com.appl.docker.dockerspring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 @SpringBootApplication
@@ -14,10 +17,28 @@ public class DockerspringApplication {
 	}
 
 
+	@Autowired
+	WebClient webClient;
+
 	@GetMapping(path = "/get")
 	public String getData() {
 
-		return "Docker spring example";
+		System.out.println("GET image");
+		return new String(getImage());
+	}
+
+
+	private byte[] getImage() {
+		byte[] image = webClient
+				.get()
+				.uri(("https://greatatmosphere.files.wordpress.com/2013/02/great-atmosphere-149-tenaya-lake-yosemite-national-park-2.jpg"))
+				.accept(MediaType.IMAGE_JPEG)
+				.retrieve()
+				.bodyToMono(byte[].class)
+				.block();
+		System.out.println(" image = " + image);
+
+		return image;
 	}
 
 }
